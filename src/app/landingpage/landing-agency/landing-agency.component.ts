6d82565a574a90876d599c3f2dd70c479c6983e7
Shanmukh <sanmukswamy238@gmail.com>
2024-06-23 23:31:56 +0530

    made few changes and done every thing

--- src/app/landingpage/landing-agency/landing-agency.component.ts
+++ src/app/landingpage/landing-agency/landing-agency.component.ts
@@ -6,10 +6,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./landing-agency.component.css']
 })
 export class LandingAgencyComponent implements OnInit {
+  targetPercentage: number = 92;
+  displayedPercentage: number = 0;
+  speed: number = 1000;
 
   constructor() { }
 
   ngOnInit(): void {
+    this.animatePercentage();
   }
-
+  animatePercentage(): void {
+    const increment = this.targetPercentage / this.speed;
+    const updateCount = () => {
+      if (this.displayedPercentage < this.targetPercentage) {
+        this.displayedPercentage = Math.min(this.targetPercentage, Math.ceil(this.displayedPercentage + increment));
+        setTimeout(updateCount, 20);
+      } else {
+        this.displayedPercentage = this.targetPercentage;
+      }
+    };
+    updateCount();
+  }
 }
